1e18bef06d791d8f6c181c048bda760c6780b6c9
Rajesh Rathore <rajeshrathore05011998@gmail.com>
2022-08-05 13:43:44 +0530

    feat:<Array> array basic operation add

--- src/Modules/Data-structures/Arrays/array.ts
+++ src/Modules/Data-structures/Arrays/array.ts
@@ -1,3 +1,5 @@
+import { DeleteFromArray } from "../../../Shared/Interface/interface";
+
 export class Array {
   createArray(array: number[]): number[] {
     return array;
@@ -10,6 +12,62 @@ export class Array {
 
   insertAtEnd(array: number[], insertElement: number): number[] {
     array.push(insertElement);
+    return array;
+  }
+
+  insertAtPosition(array: number[], index: number, insertElement: number): number[] {
+    array.splice(index, 0, insertElement);
+    return array;
+  }
+
+  removeAtfirst(array: number[]): DeleteFromArray {
+    return {
+      deletedItem: array[0],
+      array: array.splice(0, 1)
+    };
+  }
+
+  removeAtEnd(array: number[]): DeleteFromArray {
+    const lastIndex = array.length - 1;
+    return {
+      deletedItem: array[lastIndex],
+      array: array.splice(lastIndex, 1)
+    };
+  }
+
+  removeAtPosition(array: number[], index: number): DeleteFromArray {
+    return {
+      deletedItem: array[index],
+      array: array.splice(index, 1)
+    };
+  }
+
+  updateAtFirst(array: number[], updateElement: number): number[] {
+    array[0] = updateElement;
+    return array;
+  }
+
+  updateAtEnd(array: number[], updateElement: number): number[] {
+    array[array.length - 1] = updateElement;
+    return array;
+  }
+
+  updateAtPosition(array: number[], index: number, updateElement: number): number[] {
+    array[index] = updateElement;
+    return array;
+  }
+
+  rotateClockwise(array: number[]): number[] {
+    const element  = array[0];
+    array.splice(0, 1);
+    array.push(element);
+    return array;
+  }
+
+  rotateAntiClockwise(array: number[]): number[] {
+    const element  = array[array.length - 1];
+    array.splice(array.length - 1, 1);
+    array.push(element);
     return array;
   }
 }
