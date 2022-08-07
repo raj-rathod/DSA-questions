d69d0285f812a0c1637b803f6b30d0def3290690
Rajesh Rathore <rajeshrathore05011998@gmail.com>
2022-08-07 14:57:53 +0530

    feat:<Matrix> matrix adjaceny of element

--- src/Modules/Data-structures/Arrays/matrix.ts
+++ src/Modules/Data-structures/Arrays/matrix.ts
@@ -1,3 +1,5 @@
+import { MatrixAdjacent } from '../../../Shared/Interface/interface';
+
 export class Matrix {
   createMatrix(matrix: number[][]): number[][] {
     return matrix;
@@ -13,5 +15,77 @@ export class Matrix {
       result.push(row);
     }
     return result;
+  }
+  matrixAdjacentOfElement(
+    rowLength: number,
+    columnLength: number,
+    rowIndex: number,
+    columnIndex: number
+  ): MatrixAdjacent {
+    const initialValue = {
+      row: -1,
+      column: -1,
+    };
+
+    let matrixAdjacent: MatrixAdjacent = {
+      top: initialValue,
+      right: initialValue,
+      bottom: initialValue,
+      left: initialValue,
+      leftTopDiagonal: initialValue,
+      leftBottomDiagonal: initialValue,
+      rightTopDiagonal: initialValue,
+      rightBottomDiagonal: initialValue,
+    };
+    if (rowIndex - 1 >= 0) {
+      matrixAdjacent.top = {
+        row: rowIndex - 1,
+        column: columnIndex,
+      };
+    }
+    if (rowIndex + 1 < rowLength) {
+      matrixAdjacent.bottom = {
+        row: rowIndex + 1,
+        column: columnIndex,
+      };
+    }
+    if (columnIndex - 1 >= 0) {
+      matrixAdjacent.left = {
+        row: rowIndex,
+        column: columnIndex - 1,
+      };
+    }
+    if (columnIndex + 1 < columnLength) {
+      matrixAdjacent.right = {
+        row: rowIndex,
+        column: columnIndex + 1,
+      };
+    }
+    if (rowIndex - 1 >= 0 && columnIndex - 1 >= 0) {
+      matrixAdjacent.leftTopDiagonal = {
+        row: rowIndex - 1,
+        column: columnIndex - 1,
+      };
+    }
+    if (rowIndex + 1 < rowLength && columnIndex - 1 >= 0) {
+      matrixAdjacent.leftBottomDiagonal = {
+        row: rowIndex + 1,
+        column: columnIndex - 1,
+      };
+    }
+    if (rowIndex - 1 >= 0 && columnIndex + 1 < columnLength) {
+      matrixAdjacent.rightTopDiagonal = {
+        row: rowIndex - 1,
+        column: columnIndex + 1,
+      };
+    }
+    if (rowIndex + 1 < rowLength && columnIndex + 1 < columnLength) {
+      matrixAdjacent.rightBottomDiagonal = {
+        row: rowIndex + 1,
+        column: columnIndex + 1,
+      };
+    }
+
+    return matrixAdjacent;
   }
 }
--- src/Shared/Interface/interface.ts
+++ src/Shared/Interface/interface.ts
@@ -2,3 +2,19 @@ export interface DeleteFromArray {
   deletedItem: number;
   array: number[];
 }
+
+export interface RowColumnIndex {
+  row: number;
+  column: number;
+}
+
+export interface MatrixAdjacent {
+  top: RowColumnIndex;
+  right: RowColumnIndex;
+  bottom: RowColumnIndex;
+  left: RowColumnIndex;
+  leftTopDiagonal: RowColumnIndex;
+  leftBottomDiagonal: RowColumnIndex;
+  rightTopDiagonal: RowColumnIndex;
+  rightBottomDiagonal: RowColumnIndex;
+}
