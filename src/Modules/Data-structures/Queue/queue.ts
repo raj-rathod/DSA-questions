fd17b363a98967b01ed7b3d7ec0f5f4d1c4e54a5
Rajesh Rathore <rajeshrathore05011998@gmail.com>
2022-08-09 22:48:14 +0530

    feat:<Queue> basic queue operations

--- src/Modules/Data-structures/Queue/queue.ts
+++ src/Modules/Data-structures/Queue/queue.ts
@@ -1,1 +1,17 @@
-export class Test {}
+export class Queue {
+  enqueue(arr: number[], element: number): number[] {
+    arr.push(element);
+    return arr;
+  }
+
+  dequeue(arr: number[]): number[] {
+    if (arr.length > 0) {
+      arr.splice(0, 1);
+    }
+    return arr;
+  }
+
+  peek(arr: number[]): number {
+    return arr[0];
+  }
+}
