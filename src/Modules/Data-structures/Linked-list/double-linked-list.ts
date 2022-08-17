c8163c7f80219e0a1b3013473c5bf6d95f779061
Rajesh Rathore <rajeshrathore05011998@gmail.com>
2022-08-17 07:55:26 +0530

    feat:<Double linked list> double linked list insert basic method add

--- src/Modules/Data-structures/Linked-list/double-linked-list.ts
+++ src/Modules/Data-structures/Linked-list/double-linked-list.ts
@@ -1,7 +1,7 @@
 import { DoubleLinkNode } from '../../../Shared/class/double-link-node';
 
 export class DoubleLinkedList {
-  head: DoubleLinkNode | null;
+  head: DoubleLinkNode | any;
   currentNode: DoubleLinkNode | any;
 
   constructor() {
@@ -21,5 +21,48 @@ export class DoubleLinkedList {
         this.currentNode = newNode;
       }
     });
+  }
+
+  insertNodeAtFirst(element: number): void {
+    const newNode = new DoubleLinkNode(element);
+    this.head.prev = newNode;
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  insertNodeAtEnd(element: number): void {
+    const newNode = new DoubleLinkNode(element);
+    if(this.head === null){
+        this.head = newNode;
+    }else{
+        this.currentNode = this.head;
+        while( this.currentNode.next != null){
+            this.currentNode = this.currentNode.next;
+        }
+        newNode.prev = this.currentNode;
+        this.currentNode.next = newNode;
+    }
+  }
+
+  insertNodeAtPosition( element: number, position: number): void{
+    const newNode = new DoubleLinkNode(element);
+    if(position === 1 || this.head === null){
+        this.insertNodeAtFirst(element);
+    }else{
+        this.currentNode = this.head;
+        let count = 1;
+        while(this.currentNode.next != null){
+            if(count === position){
+                break;
+            }
+            count++;
+            this.currentNode = this.currentNode.next;
+        }
+        newNode.prev = this.currentNode.prev;
+        this.currentNode.prev.next = newNode;
+        this.currentNode.prev = newNode;
+        newNode.next = this.currentNode;
+
+    }
   }
 }
