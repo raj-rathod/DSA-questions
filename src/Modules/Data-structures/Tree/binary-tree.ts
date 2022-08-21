6346984b3f9795dfe4c998a902ff1a9413c05269
Rajesh Rathore <rajeshrathore05011998@gmail.com>
2022-08-21 19:59:07 +0530

    feat:<Binary Tree> binary tree create from array

--- src/Modules/Data-structures/Tree/binary-tree.ts
+++ src/Modules/Data-structures/Tree/binary-tree.ts
@@ -7,5 +7,18 @@ export class BinaryTree {
         this.root = null;
     }
 
-    
+    createBinaryTree(arr: number[]): void{
+        this.root = this.insertNodeAtLevel(arr, 0);  
+    }
+
+    insertNodeAtLevel(arr: number[], index: number): BinaryTreeNode{
+       let node: BinaryTreeNode| any = null;
+       if(index < arr.length){
+           node =  new BinaryTreeNode(arr[index]);
+           node.leftChild =  this.insertNodeAtLevel(arr, (2 * index) + 1);
+           node.rightChild = this.insertNodeAtLevel(arr, (2 * index) + 2);
+       }
+       return node;
+    }
+
 }
--- src/Shared/class/binary-tree-node.ts
+++ src/Shared/class/binary-tree-node.ts
@@ -1,11 +1,9 @@
 export class BinaryTreeNode {
-    parentNode: BinaryTreeNode| any;
     leftChild: BinaryTreeNode| any;
     rightChild: BinaryTreeNode| any;
     data: number;
 
     constructor(data: number){
-        this.parentNode = null;
         this.leftChild = null;
         this.rightChild = null;
         this.data = data;
