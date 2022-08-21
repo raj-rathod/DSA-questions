5dc7850dcc4cb31e337a20b3fc5bbe7f05c6d568
Rajesh Rathore <rajeshrathore05011998@gmail.com>
2022-08-21 17:06:52 +0530

    feat:<Binary Tree> binary tree node and class setup

--- src/Modules/Data-structures/Tree/binary-tree.ts
+++ src/Modules/Data-structures/Tree/binary-tree.ts
@@ -1,1 +1,11 @@
-export class Test {}
+import { BinaryTreeNode } from "../../../Shared/class/binary-tree-node";
+
+export class BinaryTree {
+    root: BinaryTreeNode | any;
+
+    constructor(){
+        this.root = null;
+    }
+
+    
+}
