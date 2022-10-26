7ecac4b084a641fbf4b8c1f72cd48c4c044d1396
Rajesh Rathore <rajeshrathore05011998@gmail.com>
2022-10-26 14:34:12 +0530

    feat:<Hash Table> hash table method add

--- src/Helper/helper.ts
+++ src/Helper/helper.ts
@@ -17,4 +17,23 @@ export class Helper {
   static minimum = (firstNumber: number, secondNumber: number): number => {
     return Math.min(firstNumber, secondNumber);
   };
+
+  static getPrime = (number: number): number => {
+    if(number % 2 === 0) {
+      number += 1;
+    }
+    while(!Helper.isPrime(number)){
+      number += 2;
+    }
+    return number;
+  };
+
+  static isPrime = (number: number): boolean =>{
+    if(number === 0 || number === 1) return false;
+    for(let i=2; i< number/2; i++){
+      if(number % i === 0) return false;
+    }
+    return true;
+  }
+
 }
